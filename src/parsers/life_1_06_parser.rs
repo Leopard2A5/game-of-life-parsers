0140507edf8c29f9adadc3d4ad795a5f469a7f1b
René Perschon <rperschon85@gmail.com>
2017-10-14 22:44:02 +0200

    added file format validation to life 1.06 parser

--- src/parsers/life_1_06_parser.rs
+++ src/parsers/life_1_06_parser.rs
@@ -1,5 +1,5 @@
 use super::Parser;
-use ::errors;
+use ::errors::{self, ErrorKind};
 use ::GameDescriptor;
 use ::default_game_descriptor::DefaultGameDescriptor;
 use std::io::{Read, BufRead, BufReader};
@@ -27,7 +27,9 @@ impl Parser for Life106Parser {
 				map_err(|err| errors::ErrorKind::IOError(err.kind()))?;
 			let line = line.trim();
 
-			if regex.is_match(line) {
+			if line.starts_with("#Life") {
+				check_file_format(line)?;
+			} else if regex.is_match(line) {
 				let coords: Vec<i16> = line.split_whitespace()
 					.map(|it| it.parse::<i16>().expect("Error parsing int!"))
 					.collect();
@@ -36,6 +38,17 @@ impl Parser for Life106Parser {
 		}
 
 		Ok(Box::new(ret))
+	}
+}
+
+fn check_file_format(line: &str) -> errors::Result<()> {
+	use regex::Regex;
+
+	let regex = Regex::new("#Life\\s+1.06").expect("Invalid regex");
+	if regex.is_match(line) {
+		Ok(())
+	} else {
+		bail!(ErrorKind::InvalidFileFormat)
 	}
 }
 
@@ -62,6 +75,19 @@ mod test {
 			}
 		} else {
 			panic!("No error returned!")
+		}
+	}
+
+	#[test]
+	fn should_throw_error_on_wrong_format_annotation() {
+		let mut parser = Life106Parser::new();
+		let input = Box::new("#Life 1.05\n#P -1 -1\n.*.".as_bytes());
+		let res = parser.parse(input);
+		match res {
+			Err(errors::Error(errors::ErrorKind::InvalidFileFormat, _)) => {},
+			Err(errors::Error(x, _)) => panic!("Unexpected error {}", x),
+			_ => panic!("No error thrown!"),
+
 		}
 	}
 }
